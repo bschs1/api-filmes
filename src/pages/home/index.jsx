c199dd2e14d5291911bf8c1f12f05bd4a200614d
Bruno <buuhzik@gmail.com>
2022-07-08 04:44:05 -0300

    confirme

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,26 @@
+import { Container, MovieList, Movie } from './styles';
+
+function Home() {
+  
+
+  return (
+    <Container>
+      <h1>Movies</h1>
+      <MovieList>
+        {movies.map(movie => {
+          return (
+            <Movie key={movie.id}>
+              <a href="https://www.google.com/">
+                <img src={movie.img_url} alt={movie.title} />
+              </a>
+              <span>{movie.title}</span>
+            </Movie>
+          );
+        })}
+
+      </MovieList>
+    </Container>
+  );
+}
+
+export default Home;
